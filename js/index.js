692ef7d9f15874705592bb9e0658ab83a8dbf566
GRJesus <rod.giu@hotmail.com>
2019-08-08 20:35:17 -0300

    soma até x

---
+++ js/index.js
@@ -0,0 +1,11 @@
+//3) Faça um programa em JS que calcula e escreve a seguinte soma: soma = 1/1 + 3/2 + 5/3 + 7/4 + ... + 99/50.
+
+var sum = ("A soma das equações é = ");
+var numeric = -1;
+var answer = 0;
+
+for (var i = 1; i < 51; i++) {
+  numeric = numeric + 2;
+  answer = answer + (numeric / i);
+}
+document.body.append(sum + answer);
